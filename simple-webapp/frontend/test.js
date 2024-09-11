feebe8bb9fdf9f17e4b6cc17dbc8cf7cb2540687
Gunnar Hilling <ghilling@redhat.com>
2024-09-11 08:59:57 -0400

    Gunnar instead of Pablo

--- simple-webapp/frontend/test.js
+++ simple-webapp/frontend/test.js
@@ -5,9 +5,9 @@ greet.api = async function apiMock(name) {
     return `Hello ${name}`;
 }
 
-const input = { value: "Gunnar" }
+const input = { value: "Gunnar H" }
 const h1 = { innerHTML: "" }
 
 greet(input, h1).then(() => {
-    assert.strictEqual(h1.innerHTML, "Hello Gunnar");
+    assert.strictEqual(h1.innerHTML, "Hello Gunnar H");
 });
