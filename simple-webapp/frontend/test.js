a1499a399419075a2bc261518b1f5e5cb0e7362a
Gunnar Hilling <ghilling@redhat.com>
2024-09-11 08:57:38 -0400

    Gunnar instead of Pablo

--- simple-webapp/frontend/test.js
+++ simple-webapp/frontend/test.js
@@ -5,9 +5,9 @@ greet.api = async function apiMock(name) {
     return `Hello ${name}`;
 }
 
-const input = { value: "Pablo" }
+const input = { value: "Gunnar" }
 const h1 = { innerHTML: "" }
 
 greet(input, h1).then(() => {
-    assert.strictEqual(h1.innerHTML, "Hello Pablo");
+    assert.strictEqual(h1.innerHTML, "Hello Gunnar");
 });
